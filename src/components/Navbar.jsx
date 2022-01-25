5419fef3c5fcc648defa732ebcc3fd4d8fdd5931
dheerajarora1997 <dheeraj.arora1997@yahoo.com>
2022-01-25 15:38:08 +0530

    Navbar, NewsItem, Spinner converted to function base compoent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { useState } from 'react'
 
 import {
     Link
@@ -25,73 +24,72 @@ const modeContrast = {
     light: 'light',
     dark: 'dark',
     secondary: 'dark',
-  }
+}
 
-export class Navbar extends Component {
+const Navbar = () => {
 
-    state = {
-        themeMode: 'dark',
-    }
+    let themeMode = 'dark';
 
-    render() {
+    // state = {
+    //     themeMode: 'dark',
+    // }
 
-        const themeMode = (theme) => {
-            this.setState({
-                themeMode: theme,
-            });
-            window.document.body.classList.remove('bg-primary','bg-warning','bg-success','bg-danger','bg-info','bg-light','bg-dark','bg-secondary')
-            window.document.body.classList.add('bg-opacity-25', 'bg-'+theme);
-        }
+    // const themeMode = (theme) => {
+    //     this.setState({
+    //         themeMode: theme,
+    //     });
+    //     window.document.body.classList.remove('bg-primary', 'bg-warning', 'bg-success', 'bg-danger', 'bg-info', 'bg-light', 'bg-dark', 'bg-secondary')
+    //     window.document.body.classList.add('bg-opacity-25', 'bg-' + theme);
+    // }
 
 
-        return (
-            <>
-                <nav className={`navbar navbar-expand-lg navbar-${modeContrast[this.state.themeMode]} bg-${this.state.themeMode} bg-opacity-${modeOpacity[this.state.themeMode]}`}>
-                    <div className="container">
-                        <Link className="navbar-brand" to="/">News App</Link>
-                        <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/business">Business</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/general">General</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/health">Health</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/science">Science</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/sports">Sports</Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/technology">Technology</Link>
-                                </li>
-                            </ul>
-                            <div className="d-flex justify-content-between ms-auto">
-                                <button type="button" onClick={() => { themeMode('primary') }} className="btn rounded bg-primary p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('secondary') }} className="btn rounded bg-secondary p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('danger') }} className="btn rounded bg-danger p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('warning') }} className="btn rounded bg-warning p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('success') }} className="btn rounded bg-success p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('info') }} className="btn rounded bg-info p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('dark') }} className="btn rounded bg-dark p-2 ms-3"></button>
-                                <button type="button" onClick={() => { themeMode('light') }} className="btn rounded bg-light p-2 ms-3"></button>
-                            </div>
+    return (
+        <>
+            <nav className={`navbar navbar-expand-lg navbar-${modeContrast[themeMode]} bg-${themeMode} bg-opacity-${modeOpacity[themeMode]}`}>
+                <div className="container">
+                    <Link className="navbar-brand" to="/">News App</Link>
+                    <button className="navbar-toggler border-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/business">Business</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/entertainment">Entertainment</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/general">General</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/health">Health</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/science">Science</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/sports">Sports</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/technology">Technology</Link>
+                            </li>
+                        </ul>
+                        <div className="d-flex justify-content-between ms-auto">
+                            <button type="button" onClick={() => { themeMode('primary') }} className="btn rounded bg-primary p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('secondary') }} className="btn rounded bg-secondary p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('danger') }} className="btn rounded bg-danger p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('warning') }} className="btn rounded bg-warning p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('success') }} className="btn rounded bg-success p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('info') }} className="btn rounded bg-info p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('dark') }} className="btn rounded bg-dark p-2 ms-3"></button>
+                            <button type="button" onClick={() => { themeMode('light') }} className="btn rounded bg-light p-2 ms-3"></button>
                         </div>
                     </div>
-                </nav>
-            </>
-        )
-    }
+                </div>
+            </nav>
+        </>
+    )
 }
 
 export default Navbar
--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 
-export default class NewsItem extends Component {
-    render() {
-        let { title, description, imgUrl, newsUrl, publishedAt, author, source } = this.props
-        return (
-            <div className="card w-100 mb-4 border-0 shadow">
-                <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-secondary shadow fw-normal">
-                    {source}
-                </span>
-                <div className="card-img d-flex justify-content-center align-items-center">
-                    <img src={imgUrl} className="card-img-top" />
+const NewsItem = (props) => {
+    let { title, description, imgUrl, newsUrl, publishedAt, author, source } = props
+    return (
+        <div className="card w-100 mb-4 border-0 shadow">
+            <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-secondary shadow fw-normal">
+                {source}
+            </span>
+            <div className="card-img d-flex justify-content-center align-items-center">
+                <img src={imgUrl} className="card-img-top" />
+            </div>
+            <div className="card-body">
+                <h5 className="card-title" style={{ minHeight: '55px' }} title={title}>{title ? title : 'No Title Available'}</h5>
+                <p className="card-text text-break" style={{ minHeight: '80px' }}>{description}{description ? '...' : 'No Content Available'}</p>
+                <div className="d-flex align-items-center mb-3">
+                    <small className="text-end d-inline-block w-100">By : <span className="text-muted">{author != null ? author : 'No User Available'}</span></small>
                 </div>
-                <div className="card-body">
-                    <h5 className="card-title" style={{ minHeight: '55px' }} title={title}>{title ? title : 'No Title Available'}</h5>
-                    <p className="card-text text-break" style={{ minHeight: '80px' }}>{description}{description ? '...' : 'No Content Available'}</p>
-                    <div className="d-flex align-items-center mb-3">
-                        <small className="text-end d-inline-block w-100">By : <span className="text-muted">{author != null ? author : 'No User Available'}</span></small>
-                    </div>
-                    <div className="d-flex align-items-center justify-content-between">
-                        <small className="text-end d-inline-block">{new Date(publishedAt).toGMTString()}</small>
-                        <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">Read More</a>
-                    </div>
+                <div className="d-flex align-items-center justify-content-between">
+                    <small className="text-end d-inline-block">{new Date(publishedAt).toGMTString()}</small>
+                    <a href={newsUrl} target="_blank" className="btn btn-dark btn-sm">Read More</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+export default NewsItem
